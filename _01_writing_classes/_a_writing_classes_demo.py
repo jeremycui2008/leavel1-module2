c9c5d6d73ba796affaba9de2575b886832babf69
league <ameeralih786@gmail.com>
2023-06-02 16:59:00 -0700

    cool

--- _01_writing_classes/_a_writing_classes_demo.py
+++ _01_writing_classes/_a_writing_classes_demo.py
@@ -30,7 +30,7 @@ class Duck(Animal):
     # This is a constructor! It's called when an object of this class
     # is created. The 'self' variable is always the first input parameter
     # and is used to create instance variables and call methods.
-    def __init__(self, name):
+    def __init__(self, name,identity_fraud,crime):
 
         Duck.num_ducks_created = Duck.num_ducks_created + 1
 
@@ -38,23 +38,31 @@ class Duck(Animal):
         # the constructor
         self.new_instance_variable = 'A new instance variable!'
         self.name = name
+        self.identity_fraud=identity_fraud
+        self.crime=crime
 
         # This is a local variable and can only be used in the constructor
         new_local_variable = 'A new local variable!'
 
         # Calling a method also begins with '.self'
         self.initialize()
+        self.hell()
+        self.die()
 
     # This is a method!
     def initialize(self):
         # The instance variable can be used throughout the class
         print(self.name + ' the Duck was created!')
+    def hell(self):
+        print(self.identity_fraud + 'identity')
+    def die(self):
+        print(self.crime + 'War')
 
 
 if __name__ == '__main__':
     # TODO 1) Create an object of the Duck class, for example
     #  kenny = Duck('Kenny')
-
+    kenny=Duck('Kenny')
     # TODO 2) Add 2 more input variables into the Duck constructor
 
     # TODO 3) Create 2 instance variables in the Duck class
