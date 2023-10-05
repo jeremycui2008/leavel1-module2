1fe65191e71ffd67007241c50f5089bcfcfe9ce3
jeremycui <jeremycuid@gmail.com>
2023-10-04 19:58:56 -0700

    cool

--- _03_more_gui_apps/_b_dueling_buttons.py
+++ _03_more_gui_apps/_b_dueling_buttons.py
@@ -19,12 +19,12 @@ class dueling_buttons(tk.Tk):
     # TODO 4) Create 2 buttons next to each other, side by side, with the
     #  text "Click me!". Make sure the buttons are member variables and
     #  look at the example photo for reference.
-        self.button = tk.Button(self, text='Click me!', fg='blue',
+        self.button_left = tk.Button(self, text='Click me!', fg='blue',
                                 font=('courier new', 16, 'bold'), command=self.left_button_pressed)
-        self.button.place(relx=0.1, rely=0.5, relwidth=0.8, relheight=0.2)
-        self.button = tk.Button(self, text='Click me!', fg='red',
+        self.button_left.place(relx=0.1, rely=0.5, relwidth=0.8, relheight=0.2)
+        self.button_right = tk.Button(self, text='Click me!', fg='red',
                                 font=('courier new', 16, 'bold'), command=self.right_button_pressed)
-        self.button.place(relx=0.1, rely=0.5, relwidth=0.8, relheight=0.2)
+        self.button_right.place(relx=0.1, rely=0.5, relwidth=0.8, relheight=0.2)
     # TODO 5) For the left button, add: command=self.left_button_pressed
     #  For the right button, add: command=self.right_button_pressed
 
@@ -32,20 +32,22 @@ class dueling_buttons(tk.Tk):
     #  button to small and the right button to big. For example,
     #  self.button_left.place(LEFT_SMALL)
     #  self.button_right.place(RIGHT_BIG)
-        self.button_left.place(LEFT_SMALL)
-        self.button_right.place(RIGHT_BIG)
+
     # TODO 7) Create a method called right_button_pressed and place the left
     #  button to big and the right button to small
-        def right_button_pressed(self):
-            self.button_right.place(RIGHT_SMALL)
-            self.button_left.place(LEFT_BIG)
+    def right_button_pressed(self):
+        self.button_right.place(RIGHT_SMALL)
+        self.button_left.place(LEFT_BIG)
+    def left_button_pressed(self):
+        self.button_right.place(LEFT_SMALL)
+        self.button_left.place(RIGHT_BIG)
 
 # TODO 8) Create an if __name__ == '__main__': block
 if __name__=='__main__':
     # TODO 9) Create an object of the dueling buttons class
-    duel=dueling_buttons
-    duel.title=('hi!')
+    duel=dueling_buttons()
+    duel.title('hi!')
     # TODO 10) Set the object's width and size using the geometry method
-    duel.geometry("800,800")
+    duel.geometry('800x800')
     # TODO 11) Call the object's mainloop method
     duel.mainloop()
--- _03_more_gui_apps/_c_calculator.py
+++ _03_more_gui_apps/_c_calculator.py
@@ -6,3 +6,10 @@ import tkinter as tk
 # TODO Make a calculator app like the one shown in the calculator.png image
 #  located in this folder.
 #  HINT: you'll need: 2 text fields, 4 buttons, and 1 label
+class calculator(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.multiplication = tk.Button(self, text="hi there!")
+        self.addition = tk.Button()
+        self.subtraction = tk.button()
+        self.division = tk.button()
